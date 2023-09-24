94f43ee5a22d21628e26bc0c3de7747c77af812f
judithmarg <judithmargaritapacollalli@gmail.com>
2023-09-24 13:38:14 -0400

    add home and search

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,7 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import SearchScreen from "./screens/SearchScreen";
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,24 +11,57 @@ const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator  screenOptions={{
+            tabBarStyle:{
+                backgroundColor:"rgba(0,0,0,0.5)",
+                position:"absolute",
+                bottom:0,
+                left:0,
+                right:0,
+                shadowOpacity:4,
+                shadowRadius:4,
+                elevation: 4,
+                shadowOffset:{
+                    width:0,
+                    height:-4,
+                },
+                borderTopWidth:0
+            }
+        }}>
             <Tab.Screen 
             name="Home" 
             component={HomeScreen} 
             options={{ 
                 tabBarLabel:"Home",
                 headerShown: false,
-                tabBarLabelStyle: { color: "white" },
+                tabBarLabelStyle: { color: "silver" },
                 tabBarIcon:({focused}) =>
                 focused ? (
-                    <Ionicons name="home" size={24} color="black"/>
+                    <Ionicons name="home" size={30} color="white"/>
                 ) : (
-                    <Ionicons name="heart-outline" size={30} color="black"/>
+                    <Ionicons name="home" size={30} color="silver"/>
                 ),
                 }} 
             />
+            
+            <Tab.Screen
+            name="Search"
+            component={SearchScreen}
+            options={{
+                tabBarLabel:"Search",
+                headerShown: false,
+                tabBarLabelStyle: { color: "silver" },
+                tabBarIcon:({focused}) =>
+                focused ? (
+                    <Ionicons name="search" size={30} color="white"/>
+                ) : (
+                    <Ionicons name="search" size={30} color="silver"/>
+                ),
+                
+            }}
+            />
         </Tab.Navigator>
-    )
+    );
 }
 
 const Stack = createNativeStackNavigator();

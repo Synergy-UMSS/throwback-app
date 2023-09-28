addcdcc6b943fd3cad104afcbe43527d97217b96
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2023-09-28 17:18:54 -0400

    add fonts and logohumilde

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,11 +1,11 @@
-import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
-import {RootStackParamList} from '../utils/types';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native'; // Importa Image y StyleSheet
+import { RootStackParamList } from '../utils/types';
 
 type SplashProps = NativeStackScreenProps<RootStackParamList, 'Splash'>;
 
-const Splash = ({navigation}: SplashProps) => {
+const Splash = ({ navigation }: SplashProps) => {
   useEffect(() => {
     setTimeout(() => {
       navigation.replace('Home');
@@ -13,17 +13,32 @@ const Splash = ({navigation}: SplashProps) => {
   }, [navigation]);
 
   return (
-    <View>
-      <Text
-        style={{
-            fontSize: 40,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            marginTop: 200,
-        }}
-      >Logo Fachero</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/logo.png')}
+        style={styles.logo}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    backgroundColor: '#E4E6DC', // Establece el color de fondo deseado
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
 
 export default Splash;

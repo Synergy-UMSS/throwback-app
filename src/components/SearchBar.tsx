b7dced088b26c63f931bde65469e3025debd619f
Mateo <mateovaldivia3000@gmail.com>
2023-09-29 23:19:45 -0400

    SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { View, TextInput, TouchableOpacity } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Feather from 'react-native-vector-icons/Feather';
+import * as Animatable from 'react-native-animatable';
 
 const SearchBar = () => {
   // use state for search text
@@ -12,46 +13,68 @@ const SearchBar = () => {
       <View
         style={{
           flexDirection: 'row',
-          justifyContent: 'space-between',
           alignItems: 'center',
           padding: 10,
           height: 50,
-          alignContent: 'center',
         }}
       >
+        {busqueda!=='' && (
         <TouchableOpacity>
+          <Animatable.View
+            animation={'fadeIn'}
+            duration={300} // Duración de la animación en milisegundos
+          >
           <MaterialIcons name="arrow-back" size={30} color="gray" />
-        </TouchableOpacity>
-        <TextInput
-          onChangeText={(cambio) => {
-            setBusqueda(cambio);
-          }}
-          onSubmitEditing={() => {
-            console.log("bebita bebe liean  "+busqueda);
-          }}
-          value={busqueda} 
+          </Animatable.View>
+        </TouchableOpacity>)}
+        <View
           style={{
+            flex: 1, // Utiliza flex para que el TextInput tome todo el espacio disponible
             borderWidth: 1,
             borderColor: 'gray',
             borderRadius: 10,
-            width: '80%',
             height: 40,
             paddingLeft: 10,
             paddingRight: 10,
-            fontSize: 20,
-          }}
-          placeholder="Buscar Música"
-        />
-        <TouchableOpacity
-          onPress={() => {
-            setBusqueda(''); // Limpiar el estado al presionar el botón
+            fontSize: 10,
+            flexDirection: 'row',
+            alignItems: 'center', // Alinea verticalmente el contenido del View
           }}
         >
-          <Feather name="x-circle" size={30} color="gray" />
-        </TouchableOpacity>
+          <TextInput
+            onChangeText={(cambio) => {
+              setBusqueda(cambio);
+            }}
+            onSubmitEditing={() => {
+              console.log("bebita bebe liean  " + busqueda);
+            }}
+            value={busqueda}
+            placeholder="¿Qué es lo que quieres escuchar?"
+          />
+          {busqueda !== '' && ( // Muestra el ícono de "X" solo si hay texto en el campo
+            <TouchableOpacity
+              onPress={() => {
+                setBusqueda(''); // Limpiar el estado al presionar el botón
+              }}
+              style={{
+                marginLeft: 'auto', // Agrega margen izquierdo para separar el ícono del texto
+              }}
+            >
+              <Animatable.View
+                animation={'fadeInRight'}
+                duration={300} // Duración de la animación en milisegundos
+                style={{
+                  marginLeft: 10,
+                }}
+              >
+                <Feather name="x-circle" size={30} color="gray" />
+              </Animatable.View>
+            </TouchableOpacity>
+          )}
+        </View>
       </View>
     </View>
   );
-}
+};
 
 export default SearchBar;

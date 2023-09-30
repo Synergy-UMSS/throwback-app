cd08e8e09be61a6be2e326af48b00a01c9a17ae0
Mateo <mateovaldivia3000@gmail.com>
2023-09-30 16:59:51 -0400

    .

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import {View, TextInput, TouchableOpacity} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Feather from 'react-native-vector-icons/Feather';
 import * as Animatable from 'react-native-animatable';
@@ -16,17 +16,17 @@ const SearchBar = () => {
           alignItems: 'center',
           padding: 10,
           height: 50,
-        }}
-      >
-        {busqueda!=='' && (
-        <TouchableOpacity>
-          <Animatable.View
-            animation={'fadeIn'}
-            duration={300} // Duración de la animación en milisegundos
-          >
-          <MaterialIcons name="arrow-back" size={30} color="gray" />
-          </Animatable.View>
-        </TouchableOpacity>)}
+        }}>
+        {busqueda !== '' && (
+          <TouchableOpacity>
+            <Animatable.View
+              animation={'fadeIn'}
+              duration={300} // Duración de la animación en milisegundos
+            >
+              <MaterialIcons name="arrow-back" size={30} color="gray" />
+            </Animatable.View>
+          </TouchableOpacity>
+        )}
         <View
           style={{
             flex: 1, // Utiliza flex para que el TextInput tome todo el espacio disponible
@@ -39,14 +39,13 @@ const SearchBar = () => {
             fontSize: 10,
             flexDirection: 'row',
             alignItems: 'center', // Alinea verticalmente el contenido del View
-          }}
-        >
+          }}>
           <TextInput
-            onChangeText={(cambio) => {
+            onChangeText={cambio => {
               setBusqueda(cambio);
             }}
             onSubmitEditing={() => {
-              console.log("bebita bebe liean  " + busqueda);
+              console.log('bebita bebe liean  ' + busqueda);
             }}
             value={busqueda}
             placeholder="¿Qué es lo que quieres escuchar?"
@@ -58,15 +57,13 @@ const SearchBar = () => {
               }}
               style={{
                 marginLeft: 'auto', // Agrega margen izquierdo para separar el ícono del texto
-              }}
-            >
+              }}>
               <Animatable.View
                 animation={'fadeInRight'}
                 duration={300} // Duración de la animación en milisegundos
                 style={{
                   marginLeft: 10,
-                }}
-              >
+                }}>
                 <Feather name="x-circle" size={30} color="gray" />
               </Animatable.View>
             </TouchableOpacity>

d807de1bd3eb53812f92151de75b652853324df6
Mateo <mateovaldivia3000@gmail.com>
2023-10-02 15:56:36 -0400

    touvhable working on dots and songs

--- src/components/SongSuggestion.tsx
+++ src/components/SongSuggestion.tsx
@@ -1,27 +1,41 @@
-import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, Modal, Button } from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+  Button,
+} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const SongSuggestion = ({ songData, onOptionPress }) => {
-  const { id, title, artist, artwork,url } = songData;
+const SongSuggestion = ({songData, onOptionPress}) => {
+  const {id, title, artist, artwork, url} = songData;
   const [showOptions, setShowOptions] = useState(false);
-  const linki='./Lust_for_Life.png';
+
   const handleOptionPress = () => {
     setShowOptions(!showOptions);
   };
-
+  const handlePlayPress = () => {
+    console.log('handlePlayPress');
+  };
   return (
-    <View style={styles.container}>
-      {console.log('putaaaaaaa ' + artist)}
-      <Image source={artwork} style={styles.image} />
-      <View style={styles.textContainer}>
-        <Text style={styles.songName}>{title}</Text>
-        <Text style={styles.artistName}>{artist}</Text>
+    <TouchableOpacity onPress={handlePlayPress}>
+      {/* Envuelve toda la canción en un TouchableOpacity */}
+      <View style={styles.container}>
+        <View style={styles.songContainer}>
+          <Image source={artwork} style={styles.image} />
+          <View style={styles.textContainer}>
+            <Text style={styles.songName}>{title}</Text>
+            <Text style={styles.artistName}>{artist}</Text>
+          </View>
+        </View>
+        <TouchableOpacity onPress={handleOptionPress}>
+          {/* Los tres puntos aquí */}
+          <MaterialCommunityIcons name="dots-vertical" size={30} color="gray" />
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity onPress={handleOptionPress}>
-        <MaterialCommunityIcons name="dots-vertical" size={30} color="gray" />
-      </TouchableOpacity>
-
       {/* Modal para mostrar opciones */}
       <Modal visible={showOptions} animationType="slide" transparent={true}>
         <View style={styles.modalContainer}>
@@ -30,12 +44,15 @@ const SongSuggestion = ({ songData, onOptionPress }) => {
             <Text style={styles.songName}>{title}</Text>
             <Text style={styles.artistName}>{artist}</Text>
             <Text>¿Deseas crear una memoria musical?</Text>
-            <Button title="Crear Memoria Musical" onPress={() => onOptionPress('option1')} />
+            <Button
+              title="Crear Memoria Musical"
+              onPress={() => onOptionPress('option1')}
+            />
             <Button title="Cerrar" onPress={handleOptionPress} />
           </View>
         </View>
       </Modal>
-    </View>
+    </TouchableOpacity>
   );
 };
 
@@ -43,9 +60,15 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between', // Para separar los tres puntos del resto
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
+  },
+  songContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
   },
   image: {
     width: 60,

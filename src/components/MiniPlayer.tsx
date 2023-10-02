132550495b89c73b18151d856fed1e58b44aeb97
Mateo <mateovaldivia3000@gmail.com>
2023-10-01 20:23:28 -0400

    linters

--- src/components/MiniPlayer.tsx
+++ src/components/MiniPlayer.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
-import { View, Modal, Button, Dimensions, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
+import {
+  View,
+  Modal,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+  Image,
+  StyleSheet,
+} from 'react-native';
 import Player from '../screens/Player';
 
-const MiniPlayer = ({ navigation }) => {
-    return (
-        <View style={style.maincontainer}>
-            <TouchableOpacity style={style.container} onPress={()=>navigation.navigate(Player)}>
-                <Text> Reproduciendo canción </Text>
-            </TouchableOpacity>
-        </View>
-
-    )
-}
+const MiniPlayer = ({navigation}) => {
+  return (
+    <View style={style.maincontainer}>
+      <TouchableOpacity
+        style={style.container}
+        onPress={() => navigation.navigate(Player)}>
+        <Text> Reproduciendo canción </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default MiniPlayer;
 
 const style = StyleSheet.create({
-    maincontainer:{
-        flex:1,
-    },
-    container:{
-        flexDirection: 'row',
-        height: 50,
-        borderRadius:25,
-        backgroundColor: 'white',
-        alignItems:'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        width: '100%',
-        bottom: Dimensions.get('window').height * -1 +100,
-    },
-})
-
+  maincontainer: {
+    flex: 1,
+  },
+  container: {
+    flexDirection: 'row',
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    width: '100%',
+    bottom: Dimensions.get('window').height * -1 + 100,
+  },
+});

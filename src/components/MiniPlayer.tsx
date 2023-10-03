d71db8c0086925de584970cf28ab4ba44d143b9d
Mateo <mateovaldivia3000@gmail.com>
2023-10-03 00:09:35 -0400

    changed miniplayer text name

--- src/components/MiniPlayer.tsx
+++ src/components/MiniPlayer.tsx
@@ -22,7 +22,7 @@ const MiniPlayer = ({ navigation }) => {
         <TouchableOpacity
           style={style.button}
           onPress={() => navigation.navigate(Player)}>
-          <Text> Reproduciendo canción </Text>
+          <Text> Regresar al reproductor </Text>
         </TouchableOpacity>
       </View>
     );

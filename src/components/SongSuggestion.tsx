9b236b4a34c870f35aecd728f1f09a98cbd702da
Mateo <mateovaldivia3000@gmail.com>
2023-10-02 21:04:24 -0400

    change css

--- src/components/SongSuggestion.tsx
+++ src/components/SongSuggestion.tsx
@@ -6,32 +6,28 @@ import {
   TouchableOpacity,
   StyleSheet,
   Modal,
-  Button,
 } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useNavigation } from '@react-navigation/native'; // Importa useNavigation desde React Navigation
+import { useNavigation } from '@react-navigation/native';
 import { usePlayerStore } from '../store/playerStore';
-import TrackPlayer from 'react-native-track-player';
 
 const SongSuggestion = ({ songData, onOptionPress }) => {
   const { id, title, artist, artwork, url } = songData;
   const [showOptions, setShowOptions] = useState(false);
-  const navigation = useNavigation(); // Obtiene el objeto de navegación
+  const navigation = useNavigation();
   const { setCurrentSong } = usePlayerStore();
+
   const handleOptionPress = () => {
     setShowOptions(!showOptions);
   };
 
   const handlePlayPress = () => {
-    // Navega a la pantalla "Player" cuando se presiona la canción
     setCurrentSong(songData);
-    console.log('songData', songData);
-    navigation.navigate('Player', { songData }); // Reemplaza 'Player' con el nombre de tu pantalla "Player"
+    navigation.navigate('Player', { songData });
   };
-  
+
   return (
     <TouchableOpacity onPress={handlePlayPress}>
-      {/* Envuelve toda la canción en un TouchableOpacity */}
       <View style={styles.container}>
         <View style={styles.songContainer}>
           <Image source={artwork} style={styles.image} />
@@ -41,23 +37,29 @@ const SongSuggestion = ({ songData, onOptionPress }) => {
           </View>
         </View>
         <TouchableOpacity onPress={handleOptionPress}>
-          {/* Los tres puntos aquí */}
           <MaterialCommunityIcons name="dots-vertical" size={30} color="gray" />
         </TouchableOpacity>
       </View>
-      {/* Modal para mostrar opciones */}
       <Modal visible={showOptions} animationType="slide" transparent={true}>
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Image source={artwork} style={styles.imageSelected} />
             <Text style={styles.songName}>{title}</Text>
             <Text style={styles.artistName}>{artist}</Text>
-            <Text>¿Deseas crear una memoria musical?</Text>
-            <Button
-              title="Crear Memoria Musical"
-              onPress={() => onOptionPress('option1')}
-            />
-            <Button title="Cerrar" onPress={handleOptionPress} />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, styles.cyanButton]}
+                onPress={() => onOptionPress('option1')}
+              >
+                <Text style={styles.buttonText}>Crear Memoria Musical</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, styles.salmonButton]}
+                onPress={handleOptionPress}
+              >
+                <Text style={styles.buttonText}>Cerrar</Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Modal>
@@ -69,7 +71,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between', // Para separar los tres puntos del resto
+    justifyContent: 'space-between',
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
@@ -112,6 +114,28 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     borderRadius: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    flex: 1,
+    marginHorizontal: 5,
+  },
+  salmonButton: {
+    backgroundColor: 'salmon',
+  },
+  cyanButton: {
+    backgroundColor: '#4ADCC8',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 

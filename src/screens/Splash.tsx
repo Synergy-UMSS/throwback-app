448656befdd756ba8c0293c669b767ad153213fa
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2023-10-20 19:15:09 -0400

    "Added fonts AdigianaUI and BalooThambi"

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,6 +1,6 @@
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useEffect} from 'react';
-import {View, Image, StyleSheet} from 'react-native'; // Importa Image y StyleSheet
+import {Text,View, Image, StyleSheet} from 'react-native'; // Importa Image y StyleSheet
 import {RootStackParamList} from '../utils/types';
 
 type SplashProps = NativeStackScreenProps<RootStackParamList, 'Splash'>;
@@ -8,12 +8,13 @@ type SplashProps = NativeStackScreenProps<RootStackParamList, 'Splash'>;
 const Splash = ({navigation}: SplashProps) => {
   useEffect(() => {
     setTimeout(() => {
-      navigation.replace('Home');
-    }, 2000);
+      navigation.replace('Splash');
+    }, 8000000);
   }, [navigation]);
 
   return (
     <View style={styles.container}>
+      <Text style={styles.customFontText}>Throwback</Text>
       <Image source={require('../assets/logo-no-background.png')} style={styles.logo} />
     </View>
   );
@@ -35,6 +36,11 @@ const styles = StyleSheet.create({
   logo: {
     width: 200,
     height: 169,
+  },
+  customFontText: {
+    color:'black',
+    fontFamily: 'AdigianaUI',
+    fontSize:40,
   },
 });
 

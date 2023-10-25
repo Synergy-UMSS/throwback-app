ec6efe24802abf8d50d07ed9684f8bbfc2f05ce1
katrinherrera <katrin.hv21@gmail.com>
2023-10-25 01:34:17 -0400

    name artist buttons working mini player

--- src/components/MiniPlayer.tsx
+++ src/components/MiniPlayer.tsx
@@ -1,64 +1,74 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { MusicPlayerContext } from '../components/MusicPlayerContext';
-import TrackPlayer, { usePlaybackState, State } from 'react-native-track-player';
+import { usePlayerStore } from '../store/playerStore';
+import { MusicPlayerContext } from './MusicPlayerContext';
 
-const MiniPlayer = ({ navigation }) => {
-  const { isPlaying, playPause, currentSong } = useContext(MusicPlayerContext);
+const MiniPlayer = () => {
+    const { currentSong } = usePlayerStore();
+    const musicPlayer = useContext(MusicPlayerContext);
+    const isPlaying = musicPlayer.isPlaying;
+    const playPause = musicPlayer.playPause;
 
-  return (
-    <TouchableOpacity style={styles.container} onPress={() => navigation.navigate('Player')}>
-      <Image
-        source={
-          currentSong && currentSong.coverURL
-            ? { uri: currentSong.coverURL }
-            : require('../assets/logo.png')
-        }
-        style={styles.coverImage}
-      />
-      <View style={styles.songDetails}>
-        <Text>{currentSong ? currentSong.title : 'Desconocido'}</Text>
-        <Text>{currentSong ? currentSong.artist : 'Desconocido'}</Text>
-      </View>
-      <TouchableOpacity onPress={playPause} style={styles.playPauseButton}>
-        <Ionicons
-          name={isPlaying ? 'pause-outline' : 'play-outline'}
-          size={30}
-          color="white"
-        />
-      </TouchableOpacity>
-    </TouchableOpacity>
-  );
+    return (
+        <View style={styles.miniPlayerContainer}>
+            <View style={styles.contentRow}>
+                    {currentSong.coverURL ? (
+                    <Image source={{ uri: currentSong.coverURL }} style={styles.coverImage} />
+                ) : (
+                    <Image source={require('../assets/logo.png')} style={styles.coverImage} />
+                )}
+                <View style={styles.textContainer}>
+                    <Text style={styles.songTitle} numberOfLines={1}>{currentSong.title}</Text>
+                    <Text style={styles.songArtist} numberOfLines={1}>{currentSong.artist}</Text>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.playPauseButton} onPress={playPause}>
+                <Ionicons name={isPlaying ? "pause-outline" : "play-outline"} size={30} color="white" />
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: '#96EAD2',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    width: '100%',
-    paddingHorizontal: 10,
-  },
-  coverImage: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-  },
-  songDetails: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-  },
-  playPauseButton: {
-    padding: 10, // Esto mejora la experiencia táctil
-  },
+    miniPlayerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between', 
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 15,
+        position: 'absolute',
+        left: 10,
+        right: 10,
+        bottom: 50,
+    },
+    contentRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    coverImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    textContainer: {
+        marginLeft: 10,
+        flexShrink: 1, 
+    },
+    songTitle: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    songArtist: {
+        color: 'white',
+        fontSize: 14,
+    },
+    playPauseButton: {
+        },
 });
 
 export default MiniPlayer;

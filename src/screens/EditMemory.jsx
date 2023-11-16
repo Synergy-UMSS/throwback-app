466766eb61dafeb453ecbdba81a63dcd5e83b711
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2023-11-16 13:28:06 -0400

    Update EditMemory.jsx

--- src/screens/EditMemory.jsx
+++ src/screens/EditMemory.jsx
@@ -1,37 +1,79 @@
+{/*
+  const [formData, setFormData] = useState({
+    username: 'UsuarioInicial',
+    email: 'correo@ejemplo.com',
+  });
+
+  const handleChange = (field, value) => {
+    setFormData({
+      ...formData,
+      [field]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    // Aquí puedes manejar la lógica de envío del formulario
+    console.log('Datos enviados:', formData);
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Nombre de usuario"
+        value={formData.username}
+        onChangeText={(text) => handleChange('username', text)}
+      />
+      <TextInput
+        placeholder="Correo electrónico"
+        value={formData.email}
+        onChangeText={(text) => handleChange('email', text)}
+      />
+      <Button title="Enviar" onPress={handleSubmit} />
+    </View>
+  );*/}
+
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Alert, Pressable, ScrollView } from 'react-native';
+import storage from '@react-native-firebase/storage';
+import { Image, View, Text, TextInput, StyleSheet, Alert, Pressable, Modal, ScrollView } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import firestore from '@react-native-firebase/firestore';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import firabase from '@react-native-firebase/app';
+import DateTimePicker from '@react-native-community/datetimepicker'; // Importa DateTimePicker
 import ItemSong from '../components/PreviewSong';
 import placeholderImage from '../assets/logo.png';
 import { usePlayerStore } from '../store/playerStore';
 import RequiredField from '../components/RequiredField';
 import { format } from 'date-fns';
 import EmotionPicker from '../components/EmotionPicker';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import ImagePicker from 'react-native-image-picker';
+import { PermissionsAndroid, Platform } from 'react-native';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 
+
+
+// obtener el color de la memoria basado en la emocion
+function getColorForEmotion(emotion) {
+  return emociones[emotion] || "#000000";
+}
 const emociones = {
-  emo1: "#F6EA7E",       
-  emo2: "#FBBAA4",  
-  emo3: "#C7A9D5",       
-  emo4: "#FFC1D8",     
-  emo5: "#F0CC8B",      
-  emo6: "#B6BFD4",       
-  emo7: "#FFC1D8",         
-  emo8: "#FBBAA4",   
-  emo9: "#F6EA7E",     
+  emo1: "#F6EA7E",
+  emo2: "#FBBAA4",
+  emo3: "#C7A9D5",
+  emo4: "#FFC1D8",
+  emo5: "#F0CC8B",
+  emo6: "#B6BFD4",
+  emo7: "#FFC1D8",
+  emo8: "#FBBAA4",
+  emo9: "#F6EA7E",
   emo10: "#9DE0D2",
   emo11: "#B6BFD4",
   emo12: "#F0CC8B",
   emo13: "#9DE0D2",
-  emo14: "#C7A9D5",  
+  emo14: "#C7A9D5",
 };
-
-function getColorForEmotion(emotion) {
-  return emociones[emotion] || "#000000";
-}
-
-function aclararColor(hex, porcentaje=0.5) {
+// aclarar un color hexadecimal
+function aclararColor(hex, porcentaje = 0.5) {
   let r = parseInt(hex.slice(1, 3), 16);
   let g = parseInt(hex.slice(3, 5), 16);
   let b = parseInt(hex.slice(5, 7), 16);
@@ -40,20 +82,28 @@ function aclararColor(hex, porcentaje=0.5) {
   b = Math.floor(b + (255 - b) * porcentaje);
   return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
 }
-
-const EditMemory = ({ memoriaSelecionada }) => {
-  console.log(memoriaSelecionada);
+const EditMemory = ({ navigation }) => {
   const { control, handleSubmit, formState: { errors } } = useForm();
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
-  const {setCurrentSong, currentSong} = usePlayerStore();
+  const { setCurrentSong, currentSong } = usePlayerStore();
 
   const [selectedEmotion, setSelectedEmotion] = useState("emo1");
   const [selectedEmotionName, setSelectedEmotionName] = useState("emo1");
   const [showName, setShowName] = useState(true);
 
-  const [isUpdatingMemory, setIsUpdatingMemory] = useState(false);
+  const [isCreatingMemory, setIsCreatingMemory] = useState(false);
   const cooldownTime = 5000;
+
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
 
   const handleEmotionSelected = (emotion) => {
     setSelectedEmotion(emotion);
@@ -61,63 +111,194 @@ const EditMemory = ({ memoriaSelecionada }) => {
     setShowName(true);
   };
 
-  const handleDescriptionChange = (newText) => {
-    const sanitizedText = newText.replace(/(\r\n|\n|\r)/g, '');
-    setDescription(sanitizedText);
-  };
-
-
-  const onSubmit = async (data) => {
-    if (isUpdatingMemory) {
-      return;
-    }
-
-    setIsUpdatingMemory(true);
-
-    const uptatedMemoria = {
-      title: data.tituloMemoria,
-      description: data.descripcionMemoria,
-      emotion: selectedEmotion,
-      memoryDate: firestore.Timestamp.fromDate(selectedDate),
-      song: parseInt(currentSong.id)
-    };
-
-    try {
-      await firestore().collection('memories').doc(memoria.id).update(updatedMemoria);
-      console.log('Memoria actualizada correctamente.');
-      onClose(); // Cierra la vista de edición después de actualizar
-    } catch (error) {
-      console.error('Error al actualizar la memoria: ', error);
-    } finally {
-      setIsUpdatingMemory(false);
-    }
-  };
-
-  const playSong = async () => {
-    navigation.navigate('Player', {currentSong, playlistFlow: false}); 
-  };
-
-  const showSuccessAlert = () => {
+  // para la alertita de eliminar
+  const confirmImageRemoval = () => {
     Alert.alert(
-      'Memoria actualizada con éxito',
-      'La memoria se ha actualizado correctamente.',
+      "Confirmación",
+      "¿Estás seguro de que deseas eliminar esta imagen?",
       [
         {
-          text: 'Aceptar',
-          onPress: () => {
-            navigation.navigate('Tus Memorias Musicales');
-          },
+          text: "Cancelar",
+          onPress: () => console.log("Cancelado"),
+          style: "cancel",
+        },
+        {
+          text: "Aceptar",
+          onPress: () => setImageUri(null),
         },
       ],
       { cancelable: false }
     );
   };
 
+  const handleDescriptionChange = (newText) => {
+    const sanitizedText = newText.replace(/(\r\n|\n|\r)/g, '');
+    setDescription(sanitizedText);
+  };
+  //para las iamgenes 
+  const [imageUri, setImageUri] = useState(null); // para almacenar el URI local de la imagen seleccionada
+
+  const selectImage = () => {
+    const options = {
+      noData: true,
+    };
+
+    launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        console.log('El usuario canceló la selección de la imagen');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.assets && response.assets.length > 0) {
+        const source = { uri: response.assets[0].uri };
+        const fileSize = response.assets[0].fileSize;  //para el tamaño del archivo
+
+
+
+        // Verificar si la extensión es jpg, jpeg o png
+        if (/\.(jpg|jpeg|png)$/i.test(source.uri)) {
+          // Verificar si el archivo es mayor de 7 MB
+          if (fileSize <= 7 * 1024 * 1024) { // 7 MB 
+            setImageUri(source.uri);
+          } else {
+            Alert.alert(
+              'Archivo demasiado grande',
+              'Por favor, elige uno menor a 7 MB.',
+              [{ text: 'Aceptar' }]
+            );
+          }
+        } else {
+          Alert.alert('Achivo no válido', 'Por favor, usa JPG, JPEG o PNG.',
+            [{ text: 'Aceptar' }]
+          );
+        }
+      }
+    });
+  };
+
+  //para subir la imagen a Firebase Storage y obtener la URL
+  const uploadImageAndGetURL = async (localUri) => {
+    if (!localUri) {
+      console.error('No se proporcionó URI para la imagen Dx');
+      return null;
+    }
+    const filename = localUri.substring(localUri.lastIndexOf('/') + 1);
+    const uploadUri = Platform.OS === 'ios' ? localUri.replace('file://', '') : localUri;
+    const storageRef = storage().ref(`images/${filename}`);
+    const task = storageRef.putFile(uploadUri);
+
+    try {
+      await task;
+      const url = await storageRef.getDownloadURL();
+      return url;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  };
+  //hasta aqui para seleccionar la imagen
+  const onSubmit = async (data) => {
+    if (isCreatingMemory) {
+      return;
+    }
+
+    setIsCreatingMemory(true);
+
+    let uploadedImageUrl = null;
+    if (imageUri) {
+      uploadedImageUrl = await uploadImageAndGetURL(imageUri);
+      if (!uploadedImageUrl) {
+        console.error('No se pudo obtener la URL de la imagen cargada');
+      }
+    }
+
+    const memoria = {
+      userKey: firabase.auth().currentUser?.uid,
+      title: data.tituloMemoria,
+      description: data.descripcionMemoria,
+      emotion: selectedEmotion,
+      createDate: firestore.Timestamp.now(),
+      memoryDate: firestore.Timestamp.fromDate(selectedDate),
+      song: parseInt(currentSong.id), //debe ser un entero
+      imageURL: uploadedImageUrl || '', // null si no hay imagen
+    };
+
+    try {
+      await firestore().collection('memories').add(memoria);
+      console.log('Memoria guardada correctamente.');
+      showSuccessAlert();
+    } catch (error) {
+      console.error('Error al guardar la memoria: ', error);
+    } finally {
+      setIsCreatingMemory(false); // Habilita la creación de memoria después del tiempo de cooldown
+    }
+  };
+
+  const playSong = async () => {
+    navigation.navigate('Player', { currentSong, playlistFlow: false });
+  };
+
+  const showSuccessAlert = () => {
+    Alert.alert(
+      'Memoria creada con éxito',
+      'La memoria se ha guardado correctamente.',
+      [
+        {
+          text: 'Aceptar',
+          onPress: () => {
+            navigation.navigate('Tus Memorias Musicales'); // Redirige a la vista "home"
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+
+    // función para solicitar permisos en tiempo de ejecución
+    const requestCameraPermission = async () => {
+      if (Platform.OS === 'android') {
+        try {
+          const granted = await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          ]);
+          if (
+            granted['android.permission.CAMERA'] === PermissionsAndroid.RESULTS.GRANTED &&
+            granted['android.permission.READ_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED &&
+            granted['android.permission.WRITE_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED
+          ) {
+            console.log('You can use the camera and photos');
+            return true;
+          } else {
+            console.log('Camera permission denied');
+            return false;
+          }
+        } catch (err) {
+          console.warn(err);
+          return false;
+        }
+      }
+      return true;
+    };
+
+    const handlePressImagePicker = async () => {
+      const hasPermissions = await requestCameraPermission();
+      if (hasPermissions) {
+        selectImage();
+      } else {
+        Alert.alert('Permisos Requeridos', 'Se necesitan permisos de cámara y almacenamiento para esta funcionalidad.'); //mensajito para el permiso
+      }
+    };
+    //hasta aqui los permisos
+
+
+
+  };
+
 
   return (
-    <View style={[styles.container, { backgroundColor: aclararColor(getColorForEmotion(selectedEmotion))}]}>
-    
-      <Text style={styles.pageTitle}>Editar memoria musical</Text>
+    <View style={[styles.container, { backgroundColor: aclararColor(getColorForEmotion(selectedEmotion)) }]}>
+
+      <Text style={styles.pageTitle}>Crear memoria musical</Text>
 
       <ScrollView>
 
@@ -130,11 +311,10 @@ const EditMemory = ({ memoriaSelecionada }) => {
               value={value}
               onChangeText={onChange}
               maxLength={50}
-              editable={false}
             />
           )}
           name="tituloMemoria"
-          defaultValue={""}
+          defaultValue=""
           rules={{
             required: 'Este campo es obligatorio',
             validate: {
@@ -164,18 +344,18 @@ const EditMemory = ({ memoriaSelecionada }) => {
             />
           )}
           name="descripcionMemoria"
-          defaultValue={""}
+          defaultValue=""
         />
 
-            <Text style={styles.label}>Fecha:</Text>
-            <TextInput
-              style={styles.input}
-              value={format(selectedDate, 'dd/MM/yyyy')}
-              onFocus={() => setShowDatePicker(true)}
-              placeholder="dd/mm/aaaa"
-              editable={true} //recordatorio cambiar a falso y usar un boton para cambiar el contenido
-            />
-            {showDatePicker && (
+        <Text style={styles.label}>Fecha:</Text>
+        <TextInput
+          style={styles.input}
+          value={format(selectedDate, 'dd/MM/yyyy')}
+          onFocus={() => setShowDatePicker(true)}
+          placeholder="dd/mm/aaaa"
+          editable={true} //recordatorio cambiar a falso y usar un boton para cambiar el contenido
+        />
+        {showDatePicker && (
           <DateTimePicker
             value={selectedDate}
             mode="date"
@@ -190,9 +370,52 @@ const EditMemory = ({ memoriaSelecionada }) => {
           />
         )}
 
+        <Text style={styles.label}>Imagen:</Text>
+        {/* Condición para mostrar el botón solo si imageUri es null */}
+        {!imageUri && (
+          <Pressable style={styles.button} onPress={selectImage}>
+            <Text style={styles.buttonText}>Añadir</Text>
+          </Pressable>
+        )}
+
+        {/* Modal para mostrar la imagen en pantalla completa */}
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={isModalVisible}
+          onRequestClose={closeModal}
+        >
+          <View style={styles.fullScreenContainer}>
+            <Pressable style={styles.backIcon} onPress={closeModal}>
+              <Ionicons name="arrow-back-outline" size={30} color="white" />    
+            </Pressable>
+            <Image source={{ uri: imageUri }} style={styles.fullScreenImage} />
+            <Pressable style={styles.closeModalButton} onPress={closeModal}>
+              <Text style={styles.closeModalButtonText}>Cerrar</Text>
+            </Pressable>
+          </View>
+        </Modal>
+
+        {/* Vista previa de la imagen con Pressable para abrir el modal */}
+        {imageUri && (
+          <View style={styles.containerImage}>
+            <View style={styles.previewContainer}>
+              <Pressable onPress={openModal}>
+                <Image source={{ uri: imageUri }} style={styles.previewImage} />
+              </Pressable>
+              <Pressable
+                style={styles.closeButton}
+                onPress={confirmImageRemoval} //alertita 
+              >
+                <Ionicons name="close-circle-outline" size={28} color="black" />
+              </Pressable>
+            </View>
+          </View>
+        )}
+
         <RequiredField style={styles.label}>Emoción:</RequiredField>
-        <EmotionPicker onEmotionChange={handleEmotionSelected}/>
-        
+        <EmotionPicker onEmotionChange={handleEmotionSelected} />
+
         <Text style={styles.label}>Canción vinculada:</Text>
         <View style={styles.marginBottom}>
           <ItemSong
@@ -204,9 +427,9 @@ const EditMemory = ({ memoriaSelecionada }) => {
         </View>
 
         <Pressable title="Crear Memoria" onPress={handleSubmit(onSubmit)} style={styles.button}>
-          <Text style={{ color: 'white', fontSize: 16, fontWeight:'bold' }}>
-            {isUpdatingMemory ? 'Actualizando Memoria...' : 'Actualizar Memoria'}
-            </Text>
+          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
+            {isCreatingMemory ? 'Creando Memoria...' : 'Crear Memoria'}
+          </Text>
         </Pressable>
 
       </ScrollView>
@@ -277,6 +500,64 @@ const styles = StyleSheet.create({
     elevation: 3,
     backgroundColor: 'black',
     alignSelf: 'center',
+  },
+
+  buttonText: {   //para el boton de añadir
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  containerImage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  previewContainer: {
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 3,
+    width: 250,
+    height: 250,
+  },
+
+  previewImage: { //para la previsualizacion
+    borderRadius: 20,
+    width: 250,
+    height: 250,
+    backgroundColor: '#ccc', // Un color de fondo en caso de que la imagen no cargue
+  },
+
+  closeButton: {
+    position: 'absolute',
+    top: 8,
+    right: 8,
+    backgroundColor: 'rgba(255, 255, 255, 0.3)', // semitransparente :o
+    borderRadius: 12,
+    padding: 3,
+  },
+
+  fullScreenContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+
+  fullScreenImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+
+  backIcon: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    padding: 10,
+    zIndex: 10, // Asegúrate de que el icono esté por encima de otros elementos
   },
 });
 

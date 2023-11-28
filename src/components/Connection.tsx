7c1f79aab2443072ce833f99e9c5ab1b30f99a30
MafersVi <ferciosavm2003@gmail.com>
2023-11-28 04:38:46 -0400

    refactorConnection

--- src/components/Connection.tsx
+++ src/components/Connection.tsx
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
 import { useConnectionGlobal } from '../helpcomponents/connectionGlobal';
 
 const Connection = () => {
-    const style = StyleSheet.create({
-        message:{
-            justifyContent:'center',
-            textAlign:'center',
-            fontFamily: 'Arial',
-        },
+  const styles = StyleSheet.create({
+    container: {
+      position: 'absolute',
+      bottom: 0,
+      height: 40,
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    message: {
+      textAlign: 'center',
+      fontFamily: 'Arial',
+    },
+  });
+
+  const { isConnected, setIsConnected } = useConnectionGlobal();
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected);
     });
 
-    const {isConnected, setIsConnected} = useConnectionGlobal();
-    
-    useEffect(()=> {
-        const unsubscribe = NetInfo.addEventListener(state => {
-            {/*console.log("Connection type", state.type);*/}
-            console.log("Is connected?", state.isConnected);
-            setIsConnected(state.isConnected);
-         });
-        return ()=> {
-            unsubscribe();
-        }
-    },[]);
-    return (
-            <View style={{position:'absolute',
-            bottom:0,
-            height:40,
-            width:'100%',
-            justifyContent:'center',
-            alignItems:'center',
-            backgroundColor: isConnected ?'#00FF0000': '#50505061',}}>
-                <Text style={style.message}>{isConnected ?'':'No es posible reproducir la música debido a\n problemas de conectividad.'}</Text>
-            </View>
+    return unsubscribe;
+  }, [setIsConnected]);
 
-    );
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: isConnected ? '#00FF0000' : '#50505061',
+        },
+      ]}
+    >
+      <Text style={styles.message}>
+        {isConnected
+          ? ''
+          : 'No es posible reproducir la música debido a\n problemas de conectividad.'}
+      </Text>
+    </View>
+  );
 };
 
 export default Connection;
-
-

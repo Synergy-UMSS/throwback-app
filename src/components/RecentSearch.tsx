bc08c1cec24ba907f9d9237759faa6b6bc76a022
MafersVi <ferciosavm2003@gmail.com>
2023-12-01 01:09:25 -0400

    refactorRecentSearch

--- src/components/RecentSearch.tsx
+++ src/components/RecentSearch.tsx
@@ -1,34 +1,36 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
-import {useSearchStore} from '../store/searchStore';
+import { useSearchStore } from '../store/searchStore';
 
-const RecentSearchItem = ({searchQuery}) => {
-  const {clearRecentSearches, deleteRecentSearch} = useSearchStore();
-  const { addRecentSearch, showHistory, showHistoryTrue, showHistoryFalse, updateCurrentSearch } = useSearchStore();
+const RecentSearchItem = ({ searchQuery }) => {
+  const {
+    clearRecentSearches,
+    deleteRecentSearch,
+    showHistory,
+    showHistoryTrue,
+    showHistoryFalse,
+    updateCurrentSearch,
+  } = useSearchStore();
 
   const handleDelete = () => {
     console.log('borrar');
     deleteRecentSearch(searchQuery);
   };
 
+  const handlePress = () => {
+    console.log('presionado ' + searchQuery);
+    showHistoryFalse();
+    updateCurrentSearch(searchQuery);
+  };
+
   return (
-    <TouchableOpacity
-      onPress={
-        () => {
-          console.log('presionado '+searchQuery);  
-          showHistoryFalse();
-          updateCurrentSearch(searchQuery);
-        }
-      }
-    >
+    <TouchableOpacity onPress={handlePress}>
       <View style={styles.container}>
         <Text style={styles.queryText}>{searchQuery}</Text>
         <TouchableOpacity
           style={styles.deleteButton}
-          onPress={() => {
-            handleDelete();
-          }}>
+          onPress={handleDelete}>
           <Feather name="x-circle" size={20} color="gray" />
         </TouchableOpacity>
       </View>
@@ -46,14 +48,14 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     margin: 10,
     height: 40,
-    borderRadius:10,
+    borderRadius: 10,
   },
   queryText: {
     fontSize: 16,
     flex: 1,
     alignSelf: 'center',
-    color:'#777',
-    marginLeft:20,
+    color: '#777',
+    marginLeft: 20,
   },
   deleteButton: {
     padding: 8,
